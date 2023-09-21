910916787f40f8ef7f436017264dd66cfe3a5c89
binish-exalture <binishb@exalture.com>
2023-09-21 18:07:15 +0530

    TestChargebackEvent of Chargeback added to Events API

--- Test/EventsAPI/Test_Events.cs
+++ Test/EventsAPI/Test_Events.cs
@@ -743,5 +743,67 @@ namespace Test.EventsAPI
             Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
                           Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
         }
+
+        [Fact]
+        public void TestChargebackEvent()
+        {
+            //Please provide the valid session id in place of 'sessionId'
+            var sessionId = "sessionId";
+            var chargeback = new Chargeback
+            {
+                user_id = "test_dotnet_chargeback_event",
+                session_id = sessionId,
+                transaction_id = "719637215",
+                order_id = "ORDER-123124124",
+                chargeback_state = "$lost",
+                chargeback_reason = "$duplicate",
+
+                merchant_profile = new MerchantProfile
+                {
+                    merchant_id = "123",
+                    merchant_category_code = "9876",
+                    merchant_name = "ABC Merchant",
+                    merchant_address = new Address
+                    {
+                        name = "Bill Jones",
+                        phone = "1-415-555-6040",
+                        address_1 = "2100 Main Street",
+                        address_2 = "Apt 3B",
+                        city = "New London",
+                        region = "New Hampshire",
+                        country = "US",
+                        zipcode = "03257"
+                    }
+                },
+                ach_return_code = "B02"
+            };
+
+            // Augment with custom fields
+            chargeback.AddField("foo", "bar");
+            Assert.Equal("{\"$type\":\"$chargeback\",\"$user_id\":\"test_dotnet_chargeback_event\",\"$session_id\":\"sessionId\"," +
+                                 "\"$order_id\":\"ORDER-123124124\",\"$transaction_id\":\"719637215\",\"$chargeback_state\":\"$lost\",\"$chargeback_reason\":\"$duplicate\"," +
+                                 "\"$merchant_profile\":{\"$merchant_id\":\"123\",\"$merchant_category_code\":\"9876\",\"$merchant_name\":\"ABC Merchant\",\"$merchant_address\":" +
+                                 "{\"$name\":\"Bill Jones\",\"$address_1\":\"2100 Main Street\",\"$address_2\":\"Apt 3B\",\"$city\":\"New London\",\"$region\":\"New Hampshire\"," +
+                                 "\"$country\":\"US\",\"$zipcode\":\"03257\",\"$phone\":\"1-415-555-6040\"}}," +
+                                 "\"$ach_return_code\":\"B02\",\"foo\":\"bar\"}",
+                                 chargeback.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = chargeback
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri!.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = chargeback,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
+        }
     }
 }
