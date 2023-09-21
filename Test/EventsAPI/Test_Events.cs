cf0f200b1aa850efcaab76e48291ccebf77de639
binish-exalture <binishb@exalture.com>
2023-09-21 15:10:48 +0530

    Added Test_events file for EventsAPI related tests

---
+++ Test/EventsAPI/Test_Events.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.EventsAPI
+{
+    internal class Class1
+    {
+    }
+}
