173e1d40a6116c9fc17c8f4f7b686c6de3b40ffb
binish-exalture <binishb@exalture.com>
2023-09-22 18:22:45 +0530

    TestGetMerchantsRequest added to PSPMerchantManagementAPI

--- Test/Test.cs
+++ Test/Test.cs
@@ -184,23 +184,7 @@ namespace Test
 
         }
 
-        [Fact]
-        public void TestGetMerchantsRequest()
-        {
-            //Please provide the valid account id in place of dummy number;
-            var accountId = "12345678";
-            //Please provide the valid api key in place of 'key'
-            var apiKey = "key";
-            var getMerchantRequest = new GetMerchantsRequest
-            {
-                AccountId = accountId
-            };
-            getMerchantRequest.ApiKey = apiKey;
-
-            Assert.Equal("https://api.sift.com/v3/accounts/" + accountId + "/psp_management/merchants",
-                         getMerchantRequest.Request.RequestUri!.ToString());
-
-        }
+        
 
         [Fact]
         public void TestCreateMerchantRequest()
