3c87de865ed3df3cb59792c6f02bd794fec345e7
binish-exalture <binishb@exalture.com>
2023-09-22 18:24:44 +0530

    TestUpdateMerchantRequest added to PSPMerchantManagementAPI

--- Test/Test.cs
+++ Test/Test.cs
@@ -188,69 +188,7 @@ namespace Test
 
         
 
-        [Fact]
-        public void TestUpdateMerchantRequest()
-        {
-            //Please provide the valid account id in place of dummy number;
-            var accountId = "12345678";
-            //Please provide the valid api key in place of 'key'
-            var apiKey = "key";
-            var updateMerchantRequest = new UpdateMerchantRequest
-            {
-                AccountId = accountId,
-                MerchantId = "test2",
-                ApiKey = apiKey,
-                Id = "test-vineeth-5",
-                Name = "Wonderful Payments Inc",
-                Description = "Wonderful Payments payment provider",
-                Address = new MerchantAddress
-                {
-                    Name = "Alany",
-                    Address1 = "Big Payment blvd, 22",
-                    Address2 = "apt, 8",
-                    City = "New Orleans",
-                    Country = "US",
-                    Phone = "0394888320",
-                    Region = "NA",
-                    ZipCode = "76830"
-
-                },
-                Category = "1002",
-                ServiceLevel = "Platinum",
-                Status = "active",
-                RiskProfile = new MerchantRiskProfile
-                {
-                    Level = "low",
-                    Score = 10
-                }
-            };
-            updateMerchantRequest.ApiKey = apiKey;
-
-            Assert.Equal(Convert.ToBase64String(Encoding.Default.GetBytes(apiKey)),
-                updateMerchantRequest.Request.Headers.Authorization!.Parameter);
-
-            Assert.Equal("https://api.sift.com/v3/accounts/" + accountId + "/psp_management/merchants/test2",
-                         updateMerchantRequest.Request.RequestUri!.ToString());
-
-
-            Assert.Equal("{\"id\":\"test-vineeth-5\"," +
-                "\"name\":\"Wonderful Payments Inc\"," +
-                "\"description\":\"Wonderful Payments payment provider\"," +
-                "\"address\":{\"name\":\"Alany\"," +
-                    "\"address_1\":\"Big Payment blvd, 22\"," +
-                    "\"address_2\":\"apt, 8\"," +
-                    "\"city\":\"New Orleans\"," +
-                    "\"region\":\"NA\"," +
-                    "\"country\":\"US\"," +
-                    "\"zipcode\":\"76830\"," +
-                    "\"phone\":\"0394888320\"}," +
-                "\"category\":\"1002\"," +
-                "\"service_level\":\"Platinum\"," +
-                "\"status\":\"active\"," +
-                "\"risk_profile\":{\"level\":\"low\",\"score\":10}}",
-                               Newtonsoft.Json.JsonConvert.SerializeObject(updateMerchantRequest));
-
-        }
+        
 
         [Fact]
         public void TestGetMerchantDetailsRequest()
