af552ded9c56779a066609936c7a98e2e4dd8d05
binish-exalture <binishb@exalture.com>
2023-09-22 18:37:18 +0530

    TestWebHookValidation added to CustomEvents

--- Test/CustomEvents/Test_CustomEvents.cs
+++ Test/CustomEvents/Test_CustomEvents.cs
@@ -1,7 +1,9 @@
 ﻿using Sift;
+using Sift.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -30,6 +32,32 @@ namespace Test.CustomEvents
             Assert.Equal("{\"$type\":\"make_call\",\"$user_id\":\"gary\",\"foo\":" +
                               "\"bar\",\"payment_status\":\"$success\"}",
                               makeCall.ToJson());
+        }
+
+        [Fact]
+        public void TestWebHookValidation()
+        {
+            //Please provide the valid secret key in place of 'key'
+            String secretKey = "key";
+            String requestBody = "{" +
+                "\"entity\": {" +
+                "\"type\": \"user\"," +
+                "\"id\": \"USER123\"" +
+                "}," +
+                "\"decision\": {" +
+                "\"id\": \"block_user_payment_abuse\"" +
+                "}," +
+                "\"time\": 1461963439151" +
+                "}";
+            byte[] key = Encoding.ASCII.GetBytes(secretKey);
+            HMACSHA1 myhmacsha1 = new HMACSHA1(key);
+            byte[] byteArray = Encoding.ASCII.GetBytes(requestBody);
+            MemoryStream stream = new MemoryStream(byteArray);
+            string signatureCore = myhmacsha1.ComputeHash(stream).Aggregate("", (s, e) => s + String.Format("{0:x2}", e), s => s);
+            String signature = "sha1=" + signatureCore;
+            WebhookValidator webhook = new WebhookValidator();
+            Assert.True(webhook.IsValidWebhook(requestBody, secretKey, signature));
+
         }
     }
 }
--- Test/Test.cs
+++ Test/Test.cs
@@ -36,31 +36,7 @@ namespace Test
         
 
 
-        [Fact]
-        public void TestWebHookValidation()
-        {
-            //Please provide the valid secret key in place of 'key'
-            String secretKey = "key";
-            String requestBody = "{" +
-                "\"entity\": {" +
-                "\"type\": \"user\"," +
-                "\"id\": \"USER123\"" +
-                "}," +
-                "\"decision\": {" +
-                "\"id\": \"block_user_payment_abuse\"" +
-                "}," +
-                "\"time\": 1461963439151" +
-                "}";
-            byte[] key = Encoding.ASCII.GetBytes(secretKey);
-            HMACSHA1 myhmacsha1 = new HMACSHA1(key);
-            byte[] byteArray = Encoding.ASCII.GetBytes(requestBody);
-            MemoryStream stream = new MemoryStream(byteArray);
-            string signatureCore = myhmacsha1.ComputeHash(stream).Aggregate("", (s, e) => s + String.Format("{0:x2}", e), s => s);
-            String signature = "sha1=" + signatureCore;
-            WebhookValidator webhook = new WebhookValidator();
-            Assert.True(webhook.IsValidWebhook(requestBody, secretKey, signature));
-
-        }
+        
 
         [Fact]
         public void TestWebHookValidationForInvalidSecretKey()
