a78603a582a33f3cb72ea39fb6fd34038b1f5675
binish-exalture <binishb@exalture.com>
2023-09-22 18:14:32 +0530

    TestApplyDecisionRequest added to DecisionsAPI

--- Test/Test.cs
+++ Test/Test.cs
@@ -14,27 +14,7 @@ namespace Test
 {
     public class Test
     {
-        [Fact]
-        public void TestApplyDecisionRequest()
-        {
-            var applyDecisionRequest = new ApplyOrderDecisionRequest
-            {
-                AccountId = "123",
-                UserId = "gary",
-                OrderId = "1",
-                DecisionId = "abc",
-                Source = "AUTOMATED_RULE"
-            };
-
-            applyDecisionRequest.ApiKey = "key";
-
-            Assert.Equal("https://api.sift.com/v3/accounts/123/users/gary/orders/1/decisions",
-                         applyDecisionRequest.Request.RequestUri!.ToString());
-
-            Assert.Equal(Convert.ToBase64String(Encoding.Default.GetBytes("key")),
-                         applyDecisionRequest.Request.Headers.Authorization!.Parameter);
-
-        }        
+               
 
         [Fact]
         public void TestCustomEventRequest()
