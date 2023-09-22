f9c01002ea7ed1fcdd4329424417693bc91c93f7
binish-exalture <binishb@exalture.com>
2023-09-22 18:18:34 +0530

    TestGetScoreRequest added to ScoreAPI

---
+++ Test/ScoreAPI/Test_Scores.cs
@@ -0,0 +1,24 @@
+﻿using Sift;
+using Xunit;
+
+namespace Test.ScoreAPI
+{
+    public class Test
+    {
+        [Fact]
+        public void TestGetScoreRequest()
+        {
+            ScoreRequest scoreRequest = new ScoreRequest
+            {
+                UserId = "123",
+                ApiKey = "345",
+                AbuseTypes = new List<string>() { "payment_abuse", "promotion_abuse" }
+            };
+
+            var url = scoreRequest.Request.RequestUri!.ToString();
+
+            Assert.Equal("https://api.sift.com/v205/users/123/score?api_key=345&abuse_types=payment_abuse,promotion_abuse",
+                        Uri.UnescapeDataString(scoreRequest.Request.RequestUri!.ToString()));
+        }
+    }
+}
--- Test/Test.cs
+++ Test/Test.cs
@@ -377,21 +377,7 @@ namespace Test
 
         
 
-        [Fact]
-        public void TestGetScoreRequest()
-        {
-            ScoreRequest scoreRequest = new ScoreRequest
-            {
-                UserId = "123",
-                ApiKey = "345",
-                AbuseTypes = new List<string>() { "payment_abuse", "promotion_abuse" }
-            };
-
-            var url = scoreRequest.Request.RequestUri!.ToString();
-
-            Assert.Equal("https://api.sift.com/v205/users/123/score?api_key=345&abuse_types=payment_abuse,promotion_abuse",
-                        Uri.UnescapeDataString(scoreRequest.Request.RequestUri!.ToString()));
-        }
+        
     }
 
 }
