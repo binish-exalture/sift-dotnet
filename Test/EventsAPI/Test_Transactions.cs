961876aa601e4f9f7f4d6ef74ed1deba3c141ec2
binish-exalture <binishb@exalture.com>
2023-09-22 20:50:44 +0530

    Added Test_Transactions for transaction related tests

---
+++ Test/EventsAPI/Test_Transactions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.EventsAPI
+{
+    public class Test_Transactions
+    {
+
+    }
+}
