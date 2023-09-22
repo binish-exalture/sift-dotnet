f52f4d18e3380eba388d6feab62700e3c94b4948
binish-exalture <binishb@exalture.com>
2023-09-22 18:30:28 +0530

    TestVerificationCheckRequest added to VerificationAPI

--- Test/Test.cs
+++ Test/Test.cs
@@ -49,29 +49,7 @@ namespace Test
 
         
 
-        [Fact]
-        public void TestVerificationCheckRequest()
-        {
-            //Please provide the valid api key in place of 'key'
-            var apiKey = "key";
-            var verificationCheckRequest = new VerificationCheckRequest
-            {
-
-                ApiKey = apiKey,
-                Code = 655543,
-                UserId = "haneeshv@exalture.com",
-                VerifiedEntityId = "SOME_SESSION_ID",
-                VerifiedEvent = "$login"
-            };
-
-            verificationCheckRequest.ApiKey = apiKey;
-
-            Assert.Equal(Convert.ToBase64String(Encoding.Default.GetBytes(apiKey)),
-                verificationCheckRequest.Request.Headers.Authorization!.Parameter);
-
-            Assert.Equal("https://api.sift.com/v1/verification/check",
-                         verificationCheckRequest.Request.RequestUri!.ToString());
-        }
+        
 
         [Fact]
         public void TestVerificationSendRequest()
--- Test/VerificationAPI/Test_Verifications.cs
+++ Test/VerificationAPI/Test_Verifications.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using Sift;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Test.VerificationAPI
 {
     public class Test
     {
+        [Fact]
+        public void TestVerificationCheckRequest()
+        {
+            //Please provide the valid api key in place of 'key'
+            var apiKey = "key";
+            var verificationCheckRequest = new VerificationCheckRequest
+            {
 
+                ApiKey = apiKey,
+                Code = 655543,
+                UserId = "haneeshv@exalture.com",
+                VerifiedEntityId = "SOME_SESSION_ID",
+                VerifiedEvent = "$login"
+            };
+
+            verificationCheckRequest.ApiKey = apiKey;
+
+            Assert.Equal(Convert.ToBase64String(Encoding.Default.GetBytes(apiKey)),
+                verificationCheckRequest.Request.Headers.Authorization!.Parameter);
+
+            Assert.Equal("https://api.sift.com/v1/verification/check",
+                         verificationCheckRequest.Request.RequestUri!.ToString());
+        }
     }
 }
