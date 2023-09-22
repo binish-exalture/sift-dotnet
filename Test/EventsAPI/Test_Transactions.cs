858ac874252e385de2f652f328c729bb8b13ee6f
binish-exalture <binishb@exalture.com>
2023-09-22 20:53:02 +0530

    TestTransactionEvent of Transaction added toTest_Transactions

--- Test/EventsAPI/Test_Transactions.cs
+++ Test/EventsAPI/Test_Transactions.cs
@@ -1,13 +1,53 @@
-﻿using System;
+﻿using Sift;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Test.EventsAPI
 {
     public class Test_Transactions
     {
+        [Fact]
+        public void TestTransactionEvent()
+        {
+            //Please provide the valid session id in place of 'sessionId'
+            var sessionId = "sessionId";
+            var transaction = new Transaction
+            {
+                user_id = "test_dotnet_transaction_event",
+                amount = 1000000000000L,
+                currency_code = "USD",
+                session_id = sessionId,
+                transaction_type = "$sale",
+                transaction_status = "$failure",
+                decline_category = "$invalid"
+            };
 
+            // Augment with custom fields
+            transaction.AddField("foo", "bar");
+            Assert.Equal("{\"$type\":\"$transaction\",\"$user_id\":\"test_dotnet_transaction_event\",\"$session_id\":\"sessionId\"," +
+                                 "\"$transaction_type\":\"$sale\",\"$transaction_status\":\"$failure\",\"$amount\":1000000000000,\"$currency_code\":\"USD\"," +
+                                 "\"$decline_category\":\"$invalid\",\"foo\":\"bar\"}", transaction.ToJson());
+
+            EventRequest eventRequest = new EventRequest
+            {
+                Event = transaction
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri!.ToString());
+
+            eventRequest = new EventRequest
+            {
+                Event = transaction,
+                AbuseTypes = { "legacy", "payment_abuse" },
+                ReturnScore = true
+            };
+
+            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
+                          Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
+        }
     }
 }
