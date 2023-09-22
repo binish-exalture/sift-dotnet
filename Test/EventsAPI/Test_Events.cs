538515861fae921d15955867c1a72245c9f4c227
binish-exalture <binishb@exalture.com>
2023-09-22 22:05:31 +0530

    TestLoginEvent of login added to Logging

--- Test/EventsAPI/Test_Events.cs
+++ Test/EventsAPI/Test_Events.cs
@@ -30,58 +30,7 @@ namespace Test.EventsAPI
 
         
 
-        [Fact]
-        public void TestLoginEvent()
-        {
-            //Please provide the valid session id in place of 'sessionId'
-            var sessionId = "sessionId";
-            var login = new Login
-            {
-                user_id = "test_dotnet_login_event",
-                session_id = sessionId,
-                user_email = "bill@gmail.com",
-                login_status = "$success",
-                ip = "128.148.1.135",
-                failure_reason = "$account_unknown",
-                social_sign_on_type = "$facebook",
-                username = "test_user_name",
-                site_country = "US",
-                site_domain = "sift.com",
-                brand_name = "sift",
-                browser = new Browser
-                {
-                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-                    accept_language = "en-US",
-                    content_language = "en-GB"
-                },
-                account_types = new ObservableCollection<string>() { "merchant", "premium" }
-            };
-
-            Assert.Equal("{\"$type\":\"$login\",\"$user_id\":\"test_dotnet_login_event\",\"$session_id\":\"sessionId\"," +
-                                 "\"$user_email\":\"bill@gmail.com\",\"$login_status\":\"$success\",\"$failure_reason\":\"$account_unknown\"," +
-                                 "\"$social_sign_on_type\":\"$facebook\",\"$username\":\"test_user_name\",\"$ip\":\"128.148.1.135\",\"$browser\":" +
-                                 "{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) " +
-                                 "Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":" +
-                                 "\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\",\"$account_types\":[\"merchant\",\"premium\"]}",
-                                 login.ToJson());
-
-            EventRequest eventRequest = new EventRequest
-            {
-                Event = login
-            };
-
-            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri!.ToString());
-
-            eventRequest = new EventRequest
-            {
-                Event = login,
-                AbuseTypes = { "legacy", "payment_abuse" },
-                ReturnScore = true
-            };
-
-            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
-                          Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
-        }
+        
         
 
         //UpdateContentComment
@@ -935,49 +884,6 @@ namespace Test.EventsAPI
 
         
 
-        [Fact]
-        public void TestUpdatePasswordEvent()
-        {
-            var updatePassword = new UpdatePassword
-            {
-                user_id = "billy_jones_301",
-                reason = "$forced_reset",
-                status = "$success",
-                session_id = "gigtleqddo84l8cm15qe4il",
-                ip = "128.148.1.135",
-                browser = new Browser
-                {
-                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-                    accept_language = "en-US",
-                    content_language = "en-GB"
-                },
-                brand_name = "sift",
-                site_country = "US",
-                site_domain = "sift.com",
-                user_email = "billjones1@example.com",
-                verification_phone_number = "+123456789012"
-            };
-
-            string updatePasswordBody = "{\"$type\":\"$update_password\",\"$user_id\":\"billy_jones_301\",\"$session_id\":\"gigtleqddo84l8cm15qe4il\",\"$reason\":\"$forced_reset\",\"$status\":\"$success\",\"$ip\":\"128.148.1.135\",\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$brand_name\":\"sift\",\"$site_country\":\"US\",\"$site_domain\":\"sift.com\",\"$user_email\":\"billjones1@example.com\",\"$verification_phone_number\":\"+123456789012\"}";
-
-            Assert.Equal(updatePasswordBody, updatePassword.ToJson());
-
-            EventRequest eventRequest = new EventRequest
-            {
-                Event = updatePassword
-            };
-
-            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri!.ToString());
-
-            eventRequest = new EventRequest
-            {
-                Event = updatePassword,
-                AbuseTypes = { "legacy", "payment_abuse" },
-                ReturnScore = true
-            };
-
-            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
-                          Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
-        }
+        
     }
 }
