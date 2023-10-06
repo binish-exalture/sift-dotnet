d6a671bca09c3a8c01db8968b89d2b0a2afa394b
binish-exalture <binishb@exalture.com>
2023-10-06 18:25:32 +0530

    FlagContentEvent added to Contents

--- Test/EventsAPI/FlagContent.cs
+++ Test/EventsAPI/FlagContent.cs
@@ -5,46 +5,6 @@ namespace Test.EventsAPI
 {
     public class FlagContent
     {
-        [Fact]
-        public void TestFlagContentEvent()
-        {
-            var flagContent = new Sift.FlagContent
-            {
-                user_id = "billy_jones_301",
-                session_id = "gigtleqddo84l8cm15qe4il",
-                content_id = "9671500641",
-                flagged_by = "jamieli89",
-                reason = "$toxic",
-                user_email = "billjones1@example.com",
-                browser = new Browser
-                {
-                    user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
-                    accept_language = "en-US",
-                    content_language = "en-GB"
-                },
-                verification_phone_number = "+123456789012"
-            };
-
-            string flagcontentbody = "{\"$type\":\"$flag_content\",\"$user_id\":\"billy_jones_301\",\"$session_id\":\"gigtleqddo84l8cm15qe4il\",\"$content_id\":\"9671500641\",\"$flagged_by\":\"jamieli89\",\"$reason\":\"$toxic\",\"$user_email\":\"billjones1@example.com\",\"$browser\":{\"$user_agent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36\",\"$accept_language\":\"en-US\",\"$content_language\":\"en-GB\"},\"$verification_phone_number\":\"+123456789012\"}";
-
-            Assert.Equal(flagcontentbody, flagContent.ToJson());
-
-            EventRequest eventRequest = new EventRequest
-            {
-                Event = flagContent
-            };
-
-            Assert.Equal("https://api.sift.com/v205/events", eventRequest.Request.RequestUri!.ToString());
-
-            eventRequest = new EventRequest
-            {
-                Event = flagContent,
-                AbuseTypes = { "legacy", "payment_abuse" },
-                ReturnScore = true
-            };
-
-            Assert.Equal("https://api.sift.com/v205/events?abuse_types=legacy,payment_abuse&return_score=true",
-                          Uri.UnescapeDataString(eventRequest.Request.RequestUri!.ToString()));
-        }
+        
     }
 }
